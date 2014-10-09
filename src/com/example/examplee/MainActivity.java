e1fc9cd403ae0e01e14777a711878889c27bdaef
dtaivan <taivand7@yahoo.com>
2014-10-09 11:29:43 +0800

    edit

--- src/com/example/examplee/MainActivity.java
+++ src/com/example/examplee/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.examplee;
 
+
+
+
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.support.v4.app.Fragment;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity  {
+public class MainActivity extends Activity {
 	//test https://www.youtube.com/watch?v=ZK20jVt7XEc
 	//smoke 
 	private EditText  username=null;
@@ -35,11 +44,13 @@ public class MainActivity extends Activity  {
     public void login(View view){
         if(username.getText().toString().equals("admin") && 
         password.getText().toString().equals("admin")){
-        Toast.makeText(getApplicationContext(), "Redirecting...dfdddddddd", 
+        Toast.makeText(getApplicationContext(), "Redirecting...", 
         Toast.LENGTH_SHORT).show();
+        RelativeLayout rl = (RelativeLayout)findViewById(R.id.your_layout_id);
+        rl.setBackgroundColor(Color.DKGRAY);
         counter++;
      }  else{
-         Toast.makeText(getApplicationContext(), "Wrong Credentials     ",
+         Toast.makeText(getApplicationContext(), "Wrong Credentials ",
         	      Toast.LENGTH_SHORT).show();
         	      attempts.setBackgroundColor(Color.RED);	
         	      counter--;
@@ -62,18 +73,29 @@ public class MainActivity extends Activity  {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-        	MainActivity.setBackgroundColor(android.graphics.Color.RED);
-        	
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+       
         
+        switch (item.getItemId()) {
+        case R.id.item1:
+        	PlaceholderFragment d=new PlaceholderFragment();
+        	d.onClick(null);
+            return true;
+        case R.id.item2:
+        	MainActivity.setBackgroundColor(android.graphics.Color.RED);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+    }
        
     }
+	
 	private static void setBackgroundColor(int red) {
 		// TODO Auto-generated method stub
 		
+	}
+	public static class PlaceholderFragment extends Fragment {
+		public void onClick(View arg0) {
+			Intent tableIntent = new Intent(getActivity(), Setting.class);
+		}
 	}
 }
---
+++ src/com/example/examplee/Setting.java
@@ -0,0 +1,15 @@
+package com.example.examplee;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Setting extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {         
+
+       super.onCreate(savedInstanceState);    
+       setContentView(R.layout.activity_setting_main);
+       //rest of the code
+   }
+}
